bb86486dcf84d35c71651d91f9e2bc6820a4515f
reaget <reaget.acc@gmail.com>
2023-08-07 15:01:58 +0300

    last updates

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// import getSvgProps from "./js/svg-props.js";
-// import SvgNest from "./js/svgnest.js";
-// import { getSvgScale, getNumber } from "./js/util/utils.js";
-
 const svgWrapper = document.querySelector(".preview");
 const unitSelect = document.querySelector(".control__select");
+
+const binSize = {
+  width: 3020,
+  height: 2020,
+};
 
 const items = {
   bg: null,
@@ -16,14 +17,8 @@ const result = {
   outerRect: document.querySelector(".info--outerRect .result__info-value"),
 };
 
-const fields = {
-  width: document.querySelector(".field__item--width"),
-  height: document.querySelector(".field__item--height"),
-};
-
 const bins = document.getElementById("bins"); 
 
-let scale = 1;
 let isWorking = false;
 
 document.querySelector("#fileDialog").addEventListener("change", (event) => {
@@ -33,95 +28,76 @@ document.querySelector("#fileDialog").addEventListener("change", (event) => {
       .then((response) => response.text())
       .then((svg) => {
         const svgEl = SvgNest.parsesvg(svg);
-        result.area.innerHTML = "0";
-        result.length.innerHTML = "0";
-        result.outerRect.innerHTML = "0";
-        svgWrapper.innerHTML = "";
-        bins.innerHTML = "";
-        document.getElementById('info_efficiency').innerHTML = "";
-        document.getElementById('info_iterations').innerHTML = "";
-        document.getElementById('info_placed').innerHTML = "";
+        const option = unitSelect.options[unitSelect.selectedIndex];
+        resetResults();
+        appendSvg(svgEl);
 
-        svgWrapper.appendChild(svgEl);
-        const correctSvgProps = getCorrectSvgProps(svgEl);
-        // Original SVG
-        svgEl.setAttribute("width", correctSvgProps.svg.width);
-        svgEl.setAttribute("height", correctSvgProps.svg.height);
-        svgEl.setAttribute("viewBox", correctSvgProps.viewbox);
-        // BG RECT
-        const bgSvgRect = createBgSvgRect(svgEl);
-        svgEl.classList.add("svg-original");
-        svgWrapper.appendChild(bgSvgRect);
-        bgSvgRect.setAttribute("width", correctSvgProps.svg.width);
-        bgSvgRect.setAttribute("height", correctSvgProps.svg.height);
-
-        items.bg = bgSvgRect.querySelector("rect");
-        items.svg = svgEl;
- 
-        scale = getSvgScale(svgEl);
-
-        fields.width.value = getNumber(bgSvgRect.getAttribute("width")).toFixed(2);
-        fields.height.value = getNumber(bgSvgRect.getAttribute("height")).toFixed(2);
+        result.area.innerHTML = getArea(option);
+        result.length.innerHTML = getLength(option);
+        getOuterRect(option, (res) => {
+          result.outerRect.innerHTML = res.map((value, index) => {
+            return `<span>Опис. прямоугольник #${index}: ${convertToUnit(value, option, "outerRectResults")}</span>`;
+          }).join("");
+        })
       });
   });
 });
+
+function resetResults() {
+  result.area.innerHTML = "0";
+  result.length.innerHTML = "0";
+  result.outerRect.innerHTML = "0";
+  svgWrapper.innerHTML = "";
+  bins.innerHTML = "";
+}
+
+function appendSvg(_svg) {
+  svgWrapper.appendChild(_svg);
+
+  const correctSvgProps = getCorrectSvgProps(_svg);
+  // Original SVG
+  _svg.setAttribute("width", correctSvgProps.svg.width);
+  _svg.setAttribute("height", correctSvgProps.svg.height);
+  _svg.setAttribute("viewBox", correctSvgProps.viewbox);
+  // BG RECT
+  const bgSvgRect = createBgSvgRect(_svg);
+  _svg.classList.add("svg-original");
+  svgWrapper.appendChild(bgSvgRect);
+  bgSvgRect.setAttribute("width", correctSvgProps.svg.width);
+  bgSvgRect.setAttribute("height", correctSvgProps.svg.height);
+
+  items.bg = bgSvgRect.querySelector("rect");
+  items.svg = _svg;
+}
+
+function getLength(option) {
+  const svgProps = getSvgProps(items.svg);
+  const value = svgProps.length();
+  return convertToUnit(value, option, "length");
+}
+
+function getArea(option) {
+  const svgProps = getSvgProps(items.svg);
+  const value = svgProps.area();
+  return convertToUnit(value, option, "area");
+}
+
+function getOuterRect(option, fn) {
+  stopNesting();
+  startNesting(fn);
+}
 
 document.addEventListener("click", ({ target }) => {
   const { action } = target.dataset;
   const svg = items.svg;
   const option = unitSelect.options[unitSelect.selectedIndex];
 
-  if (!action || !svg) {
-    return;
-  }
-
-  if (["area", "length", "outerRect"].includes(action)) {
-    const svgProps = getSvgProps(svg);
-    const svgPropAction = svgProps[action];
-    const value = svgPropAction();
-  
-    result[action].innerHTML = convertToUnit(value, option, action);
-  }
-
   if (action === "outerRectResults") {
-    const outerRect = document.querySelector("#bins_outer-rect");
-    let output = "";
-    const results = bins.querySelectorAll("svg");
-    [...results].forEach((svg, index) => {
-      const svgProps = getSvgProps(svg);
-      const value = svgProps.outerRect();
-      // console.log(value, option, action)
-      output += `
-        <span>Опис. прямоугольник #${index}: ${convertToUnit(value, option, action)}</span>
-      `;
-      // console.log(svg, svgProps.outerRect());
-    });
-
-    outerRect.innerHTML = output;
+    
   }
 
   if (action === "nest") {
     isWorking ? stopNesting() : startNesting();
-  }
-
-  if (action === "download") {
-    downloadSvgs();
-  }
-});
-
-document.addEventListener("input", ({ target }) => {
-  if (!target.classList.contains("field__item")) {
-    return;
-  }
-
-  const value = getNumber(target.value) / scale;
-
-  if (target.classList.contains("field__item--width")) {
-    items.bg.setAttribute("width", value);
-  }
-
-  if (target.classList.contains("field__item--height")) {
-    items.bg.setAttribute("height", value);
   }
 });
 
@@ -167,7 +143,7 @@ let prevpercent = 0;
 let startTime = null;
 let iterations = 0;
 
-function startNesting() {
+function startNesting(fn) {
   // SvgNest.config({
   //   curveTolerance: "0.3",
   //   exploreConcave: false,
@@ -183,25 +159,21 @@ function startNesting() {
   iterations = 0;
 
   SvgNest.setbin(items.bg);
-  SvgNest.start(progress, renderSvg);
+  SvgNest.start(progress, (svglist, efficiency, placed, total) => {
+    renderSvg(svglist, efficiency, placed, total, fn)
+  });
   isWorking = true;
-
-  const button = document.querySelector("[data-action='nest']");
-  button.innerHTML = "Остановить";
 }
 
 function stopNesting() {
   SvgNest.stop();
   isWorking = false;
-
-  const button = document.querySelector("[data-action='nest']");
-  button.innerHTML = "Компоновать элементы";
 }
 
 function progress(percent){
   var transition = percent > prevpercent ? '; transition: width 0.1s' : '';
   document.getElementById('info_progress').setAttribute('style','width: '+Math.round(percent*100)+'% ' + transition);
-  document.getElementById('info').setAttribute('style','display: block');
+  // document.getElementById('info').setAttribute('style','display: block');
   
   prevpercent = percent;
   
@@ -212,25 +184,22 @@ function progress(percent){
     var estimate = (diff/percent)*(1-percent);
     // document.getElementById('info_time').innerHTML = millisecondsToStr(estimate)+' remaining';
     
-    if(diff > 5000 && percent < 0.3 && percent > 0.02 && estimate > 10000){
-      document.getElementById('info_time').setAttribute('style','display: block');
-    }
+    // if(diff > 5000 && percent < 0.3 && percent > 0.02 && estimate > 10000){
+    //   document.getElementById('info_time').setAttribute('style','display: block');
+    // }
   }
   
-  if(percent > 0.95 || percent < 0.02){
-    document.getElementById('info_time').setAttribute('style','display: none');
-  }
+  // if(percent > 0.95 || percent < 0.02){
+  //   document.getElementById('info_time').setAttribute('style','display: none');
+  // }
   if(percent < 0.02){
     startTime = new Date().getTime();
   }
 }
 
-function renderSvg(svglist, efficiency, placed, total) {
+function renderSvg(svglist, efficiency, placed, total, fn) {
   iterations++;
-  if (iterations >= 5) {
-    stopNesting();
-  }
-  document.getElementById('info_iterations').innerHTML = iterations;
+  // document.getElementById('info_iterations').innerHTML = iterations;
   
   if(!svglist || svglist.length == 0){
     return;
@@ -242,35 +211,29 @@ function renderSvg(svglist, efficiency, placed, total) {
     if(svglist.length > 2){
       svglist[i].setAttribute('class','grid');
     }
-    svglist[i].setAttribute("width", `${fields.width.value}mm`);
-    svglist[i].setAttribute("height", `${fields.height.value}mm`);
+    svglist[i].setAttribute("width", `${binSize.width}mm`);
+    svglist[i].setAttribute("height", `${binSize.height}mm`);
     bins.appendChild(svglist[i]);
   }
-  
-  if(efficiency || efficiency === 0){
-    document.getElementById('info_efficiency').innerHTML = Math.round(efficiency*100);
+
+  if (iterations >= 1) {
+    stopNesting();
+    let output = [];
+    const results = bins.querySelectorAll("svg");
+    // console.log(222, results);
+    [...results].forEach((svg, index) => {
+      // console.log(svg)
+      const svgProps = getSvgProps(svg);
+      const value = svgProps.outerRect();
+      output.push(value);
+    });
+    fn(output);
   }
+  
+  // if(efficiency || efficiency === 0){
+  //   document.getElementById('info_efficiency').innerHTML = Math.round(efficiency*100);
+  // }
 
-  document.getElementById('info_iterations').innerHTML = iterations;
-  document.getElementById('info_placed').innerHTML = placed+'/'+total;
-}
-
-function downloadSvgs() {  
-  const svgs = bins.querySelectorAll("svg");
-
-  [...svgs].forEach((item, index) => {
-    const svg = item.cloneNode(true);
-    svg.querySelector(".bin").remove();
-    svg.setAttribute("fill", "#fff");
-    svg.setAttribute("stroke", "#3bb34a");
-    let output;
-    if(typeof XMLSerializer != 'undefined'){
-      output = (new XMLSerializer()).serializeToString(svg);
-    } else {
-      output = svg.outerHTML;
-    }
-
-    let blob = new Blob([output], {type: "image/svg+xml;charset=utf-8"});
-    saveAs(blob, `output_${index}.svg`);
-  });
+  // document.getElementById('info_iterations').innerHTML = iterations;
+  // document.getElementById('info_placed').innerHTML = placed+'/'+total;
 }
